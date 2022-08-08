6ab026eb72fc60ea4882b60f05e3b9477e02f786
Cyb3rsynth <acp46800@gmail.com>
2022-08-08 21:40:18 +0800

    Cleaned up code and comments to make everything lighter and more readable

--- javascript.js
+++ javascript.js
@@ -1,30 +1,15 @@
-// Rock paper scissors project!
-
-// Get a choice from the computer that's either 'rock', 'paper' or 'scissors'
-//We’ll use this function in the game to make the computer’s play. Tip:console to make sure this is returning the expected output before moving to the next step!
-
-
-// Get input from the user for either 'rock', 'paper' or 'scissors'
-
-// Play a round of rock paper scissors with the playerSelection and computerSelection - 
-// And then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-// Use a loop function to play the 5 rounds
-
+// Rock paper scissors project - Calling to the console log.
 console.log("Rock-Paper-Scissors! Test your might against the Computer! Best out of 5 games wins!");
 
+//Global variables for the R/P/S Project
 let playerScore = 0;
 let compScore = 0;
 let tieScore = 0;
 let playerChoice;
-//const playerChoice = getPlayerChoice();
 let computerSelection = getComputerChoice();
-const gameInputs = ["Rock", "Paper", "Scissors"];
+//const gameInputs = ["Rock", "Paper", "Scissors"];
 
-
-//Computer Play - This one is working and finalized now.
+//Function to get a random R/P/S choice from the computer. Using RNG formula
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
   if (randomNum === 0) {
@@ -36,21 +21,13 @@ function getComputerChoice() {
   }  
 }
 
-//Player Selection
-
-//function getPlayerChoice(playerChoice) {
-//result = prompt("Choose Your Fighter!");
-//return result;
-//}
-//Function works for player play
-
-//Play Round of Game Logic works within game and grabs input from player Choice
-function playRound(PlayerChoice, computerSelection) {
+//Function to play one-round of R/P/S and declare a winner or loser of the round in the console log
+function playRound(playerChoice, computerSelection) {
 
   playerChoice = prompt("Choose Your Fighter!");
   computerSelection = getComputerChoice();
   
-  if (
+    if (
      (playerChoice === 'Rock' && computerSelection === 'Scissors') ||
      (playerChoice === 'Paper' && computerSelection === 'Rock') ||
      (playerChoice === 'Scissors' && computerSelection === 'Paper')
@@ -58,7 +35,7 @@ function playRound(PlayerChoice, computerSelection) {
     playerScore+=1;
     console.log(`You Won! You chose ${playerChoice} and the Computer chose ${computerSelection}.`);
   }
-  else if (
+   else if (
     (computerSelection === 'Rock' && playerChoice === 'Scissors') ||
     (computerSelection === 'Paper' && playerChoice  === 'Rock') ||
     (computerSelection === 'Scissors' && playerChoice  === 'Paper')
@@ -66,7 +43,7 @@ function playRound(PlayerChoice, computerSelection) {
   compScore+=1;
   console.log(`LOL, You lost! The Computer chose ${computerSelection} and you chose ${playerChoice}.`);
  }
- if (
+  if (
   (computerSelection === 'Rock' && playerChoice === 'Rock') ||
   (computerSelection === 'Paper' && playerChoice  === 'Paper') ||
   (computerSelection === 'Scissors' && playerChoice  === 'Scissors')
@@ -76,11 +53,7 @@ function playRound(PlayerChoice, computerSelection) {
  }
 }
 
-//playRound(playerChoice,computerSelection);
-
-//Tested output from here and prior, codes works before game set up
-
-//function to show game winner and display final results to console log
+//Function to show game winner and display final results to console log
 function gameWinner(playerScore, compScore) {
   if(playerScore === compScore){
     console.log ("It's a draw - Play Again");
@@ -93,18 +66,11 @@ function gameWinner(playerScore, compScore) {
   }
   }
 
+//Function to play the 5 rounds of the game using a a for loop and declaring a final winner at the end, based on best / 5.
 function playGame() {
   for (let i = 0; i < 5; i++) {
   playRound(playerChoice, computerSelection);
  }
- gameWinner(playerScore, compScore); 
+  gameWinner(playerScore, compScore); 
 }
 playGame();
-
-
-
-
-
-
-
-
